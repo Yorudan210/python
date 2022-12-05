a861cc4d74d2baf957fc640a50a1e554187839fc
Jordan <jordan.hoc.62@gmail.com>
2022-12-05 13:40:01 +0100

    Suite du cour sur les variables
    Ajout de nouveaux types de données.
    Ajout d'une énigme.

--- variables.py
+++ variables.py
@@ -1,4 +1,8 @@
 # L'opérateur d'affectation = permet d'injecter une valeur dans une variable.
+foo = "lorem ipsum"
+# Affectation de valeur à partir d'une variable
+bar = foo
+print(bar)
 
 my_number1 = 123
 my_number2 = -42
@@ -11,3 +15,24 @@ my_text2 = 'Ceci est aussi une chaîne de caractères'
 
 print(my_text1)
 print(my_text2)
+
+
+# Exercice
+
+a = 123
+b = 42
+
+a,b = b, a
+print(a, b)
+
+a = 123
+b = 42
+#---------------------------------------------
+
+
+
+# Variante qui ne marche qu'avec des nombres.
+c = a + b
+a = c - a
+b = c - b
+print(a, b)
